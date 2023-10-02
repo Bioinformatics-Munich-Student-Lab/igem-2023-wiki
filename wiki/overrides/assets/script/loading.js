cf6c1e430c7387394e7b161b6d23ae3a0bdce25d
Finn Lueth <38927318+finnlueth@users.noreply.github.com>
2023-10-02 18:01:25 +0200

    fixed header vanishing

--- wiki/overrides/assets/script/loading.js
+++ wiki/overrides/assets/script/loading.js
@@ -8,15 +8,3 @@ function hideLoadingOverlay() {
     const loadingOverlay = document.querySelector(".loading-overlay");
     loadingOverlay.style.display = "none";
 }
-
-const scrollDisableDiv = document.querySelector('.scroll-disable');
-
-scrollDisableDiv.addEventListener('mouseenter', () => {
-    // Disable scrolling on hover
-    document.body.style.overflow = 'hidden';
-});
-
-scrollDisableDiv.addEventListener('mouseleave', () => {
-    // Re-enable scrolling when hover ends
-    document.body.style.overflow = 'auto';
-});
